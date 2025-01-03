449edc8e1b9a5ffa08071a509fae2c3115db7c54
dhafin <dhio@dhaf.in>
2025-01-03 17:51:19 +0700

    setup dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
+# Direktori dotfiles
 DOTFILES_DIR=~/dotfiles
 
+# Symlink file dotfiles
 ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
 ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
 ln -sf $DOTFILES_DIR/.p10k.zsh ~/.p10k.zsh
+ln -sf $DOTFILES_DIR/.config ~/.config
 
-echo "Dotfiles symlinked!"
+# Install plugins
+ZSH_CUSTOM=$ZSH/custom
+
+# zsh-autosuggestions
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions plugin..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+# zsh-syntax-highlighting
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting plugin..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+
+# you-should-use
+if [ ! -d "$ZSH_CUSTOM/plugins/you-should-use" ]; then
+    echo "Installing you-should-use plugin..."
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use $ZSH_CUSTOM/plugins/you-should-use
+fi
+
+# Selesai
+echo "Dotfiles symlinked and plugins installed!"
+
+
+
+
+
+
+
+
+
+
